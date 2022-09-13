9384b0130fe522135f1142b471d6c992469b6dc0
Drew Spampinato <113466949+drewspampy22@users.noreply.github.com>
2022-09-13 14:50:33 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main
+
+{
+    public static void main(String[] args)
+    {
+        float age= 19;
+
+        System.out.println("Your age is " + age);
+        System.out.println("Half your age is " + age /2 );
+
+    }
+}

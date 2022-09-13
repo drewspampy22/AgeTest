8e9a47671140af9b16847045f4a7aea2660ff464
Drew Spampinato <113466949+drewspampy22@users.noreply.github.com>
2022-09-13 15:02:53 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,26 @@ public class Main
 {
     public static void main(String[] args)
     {
-        float age= 19;
+        int age= 19;
 
         System.out.println("Your age is " + age);
-        System.out.println("Half your age is " + age /2 );
+        System.out.println("Half your age is " + age /2.0 );
+        System.out.println("Half your age is " + age % 2 );
+
+
+        double salary= 20000.50;
+        System.out.println("Your salary is " + salary);
+        System.out.println("Half your wage is " + salary /2.0 );
+        System.out.println("Your mod wage is " + salary % 2 );
+
+
+        String name= "Drew";
+        String lname= "Spampinato";
+
+        System.out.println("Your name is " + name + " " + lname);
+
+
 
     }
 }
+
